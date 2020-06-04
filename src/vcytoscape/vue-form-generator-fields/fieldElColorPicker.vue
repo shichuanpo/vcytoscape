19da97525e7c1f5eb4ae0ae336d9ff80f78d8c4a
zhangji <zhangji@getui.com>
2020-06-04 17:17:56 +0800

    fix: 颜色选择器中，去掉element-ui的按键和输入框样式，加上自定义样式，防止业务引用element-ui的样式冲突

--- src/vcytoscape/vue-form-generator-fields/fieldElColorPicker.vue
+++ src/vcytoscape/vue-form-generator-fields/fieldElColorPicker.vue
@@ -1,6 +1,7 @@
 
 <template lang="pug">
   el-color-picker(
+    popper-class="color-picker",
     v-model="value",
     :disabled="disabled",
     :readonly="schema.readonly",
@@ -12,11 +13,36 @@
 import { abstractField } from 'vue-form-generator'
 import { ColorPicker } from 'element-ui'
 import 'element-ui/lib/theme-chalk/color-picker.css'
-import 'element-ui/lib/theme-chalk/button.css'
-import 'element-ui/lib/theme-chalk/input.css'
+// import 'element-ui/lib/theme-chalk/button.css'
+// import 'element-ui/lib/theme-chalk/input.css'
 export default {
   name: 'fieldElColorPicker',
   mixins: [ abstractField ],
   components: { elColorPicker: ColorPicker }
 }
 </script>
+<style lang='less'>
+.color-picker {
+  .el-button--text {
+    padding: 0 15px;
+    line-height: 26px;
+    border: none;
+    background: transparent;
+    color: #01acf3;
+    &, &:focus {
+      outline: none;;
+    }
+  }
+  .el-input__inner {
+    padding: 2px 8px;
+    line-height: 20px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    transition: all 0.3s;
+    &:focus {
+      border-color: #01acf3;
+      outline: none;
+    }
+  }
+}
+</style>
